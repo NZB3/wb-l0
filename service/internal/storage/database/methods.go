31c8cac94fbb3437ef7522a065f74197baec2447
Nikolay Bukhalov <ni.bukhalov@gmail.com>
2023-07-10 14:06:00 +0300

    plug on database methods

--- service/internal/storage/database/methods.go
+++ service/internal/storage/database/methods.go
@@ -12,13 +12,19 @@ type DB interface {
 
 func (db *db) SaveOrder(order models.Order) error {
 	const op = "db.SaveOrder"
-
+	_ = op
+	return nil
 }
 
 func (db *db) GetAllOrders() ([]models.Order, error) {
 	op := "db.GetAllOrders"
+	_ = op
+	return nil, nil
 }
 
 func (db *db) GetOrder(orderUID string) (models.Order, error) {
 	op := "db.GetOrder"
+	_ = op
+
+	return models.Order{}, nil
 }
